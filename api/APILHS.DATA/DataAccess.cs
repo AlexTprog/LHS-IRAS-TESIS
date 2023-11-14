c653a5862a2d7226c6d1dde5431132a292ce1d02
Alexander Torre <alexander.torre.arteaga@gmail.com>
2023-11-14 13:47:31 -0500

    real initial commit

---
+++ api/APILHS.DATA/DataAccess.cs
@@ -0,0 +1,38 @@
+﻿using APILHS.MODELS;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace APILHS.DATA
+{
+    public class DataAccess
+    {
+        private readonly string connectionString;
+
+        public DataAccess()
+        {
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder
+            {
+                DataSource = "",
+                UserID = "",
+                Password = "",
+            };
+
+            connectionString = builder.ConnectionString;
+        }
+
+        public IDataReader InsertDiagnotico(Diagnostico diag)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string storedProcedureName = "Sp";
+                SqlCommand command = new SqlCommand(storedProcedureName, connection);
+
+                connection.Open();
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@Param", diag.Id);
+                return command.ExecuteReader();
+            }
+        }
+    }
+}
---
+++ api/APILSH/Controllers/DiagnosticoController.cs
@@ -0,0 +1,27 @@
+﻿using APILHS.DATA;
+using APILHS.MODELS;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APILHS.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DiagnosticoController
+    {
+        private readonly ILogger<Diagnostico> _logger;
+
+        public DiagnosticoController(ILogger<Diagnostico> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public IActionResult Get()
+        {
+            var data = new DataAccess();
+            var diag = data.InsertDiagnotico(new Diagnostico { Id = 1 });
+
+            return new JsonResult(diag);
+        }
+    }
+}
